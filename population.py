6aad85709cf0d2ad1d483e9b63a22e098fd90d2c
Tim <ts0928@student.uni-lj.si>
2016-12-27 20:21:01 +0100

    Dodano generiranje agentov.

--- population.py
+++ population.py
@@ -1,3 +1,8 @@
+from random import randint
+
+from simulator import get_parser, vector_to_prog
+
+
 class Population:
     def __init__(self, population_size=10, agent_size=20):
         self.population_size = population_size
@@ -5,7 +10,10 @@ class Population:
         self.agents = None
 
     def generate_agents(self):
-        pass
+        cmds = get_parser()
+        return [[randint(0, len(cmds) - 1) for _ in range(self.agent_size)]
+                for _ in range(self.population_size)]
 
     def crossover(self):
         pass
+
