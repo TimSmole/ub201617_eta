21137c3b719f1cbc563956a5d7d179399abcbf4f
Tim <ts0928@student.uni-lj.si>
2017-01-05 20:37:27 +0100

    DEBUG MODE!

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 
+import mazes
 from population import Population
+from simulator import simulate
 
 start = time.time()
 
@@ -9,6 +11,22 @@ number_of_generations = 10
 
 population = Population(population_size=population_size)
 population.generate_agents()
+
+a = population.agents[0]
+print("".join(a.to_program()))
+
+
+# FIXME: ==========REMOVE ==========
+import pylab as plt
+plt.ion()
+plt.clf()
+print(mazes.mazes_train[0])
+plt.imshow(mazes.mazes_train[0][4], cmap=plt.cm.binary, interpolation='nearest')
+plt.pause(0.01)
+
+simulate(mazes.mazes_train[0], a, verbose=False, graphics=True, delay=0.1, max_iter=10)
+exit()
+
 
 for i in range(number_of_generations):
     print("========================== GENERATION " + str(i) + " ==========================")
