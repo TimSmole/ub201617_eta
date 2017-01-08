6effdaacf2a330e5525aaade13fbda7a73cf3d88
1nfused <enigma4.speed@gmail.com>
2017-01-08 12:42:58 +0100

    Changed main into object. To be added randomized agents instead of evolution.

--- main.py
+++ main.py
@@ -6,27 +6,124 @@ from dateutil.parser import parse
 from agent import Agent
 from population import Population, Ranking
 
-population_size = 150
-agent_size = 25
-number_of_generations = 500
-elite_size = 15
+execution_break = "2017-01-08T14:00:00"
 
-execution_break = "2017-01-08T10:00:00"
+class MazeRunner(object):
+    POPULATION_SIZE = 0
+    AGENT_SIZE = 0
+    NUMBER_OF_GENERATIONS = 0
+    ELITE_SIZE = 0
 
-# FOR STATS
-best_fitness = []
-worst_fitness = []
-avg_fitness = []
+    # For stats
+    best_fitness = []
+    worst_fitness = []
+    average_fitness = []
+
+    def __init__(self, population_size, agent_size, 
+                    number_of_generations, elite_size):
+
+        self.POPULATION_SIZE = population_size
+        self.AGENT_SIZE = agent_size
+        self.NUMBER_OF_GENERATIONS = number_of_generations
+        self.ELITE_SIZE = elite_size
+
+        self.DELIMITER = "-" * 15 + "\n"
+
+    def execute(self):
+        start = time.time()
+        population = Population(population_size=self.POPULATION_SIZE, 
+                                agent_size=self.AGENT_SIZE, 
+                                elite_size=self.ELITE_SIZE,
+                                ranking=Ranking.FITNESS)
+        
+        population.generate_agents()
+        do_break_on_time, break_date_time = self.prepare_execution_break(execution_break)
+        padding = "=" * 26
+
+        i = 0
+        while i < self.NUMBER_OF_GENERATIONS \
+                and (not do_break_on_time or (do_break_on_time and datetime.now() < break_date_time)):
+            
+            print "".join((padding, (" GENERATION " + str(i) + " "), padding))
+            fit_start = time.time()
+
+            fitness = [a.async_compute_fitness() for a in population.agents]
+            print("async_compute_fitness time: " + str(time.time() - fit_start))
+
+            population_avg = sum(fitness) / len(fitness)
+            for f in fitness:
+                print("Agent fitness: " + str(f))
+            
+            agents_sorted = sorted(fitness)
+            population_best = agents_sorted[0]
+            population_worst = agents_sorted[len(agents_sorted) - 1]
+        
+            print("Best population fitness: " + str(population_best))
+            print("Worst population fitness: " + str(population_worst))
+            print("Average population fitness: " + str(population_avg))
+        
+            self.best_fitness.append(population_best)
+            self.worst_fitness.append(population_worst)
+            self.average_fitness.append(population_avg)
+
+            population.evolve(debug=False)
+            i += 1
+
+        print("".join((padding, (" FINISHED " + str(i) + " "), padding)))
+        print("Execution time: " + str(time.time() - start))
+
+        [a.async_compute_fitness() for a in population.agents]
+        best = population.return_fittest(population.agents)
+        print("==== BEST AGENT ====")
+
+        stats_path = './out/run_stats.txt'
+        self.write_stats_to_file(stats_path)
+        self.save_agent(best)
+        best.solve_mazes()
+
+    def write_stats_to_file(self, file_path):
+        # Open file
+        file = open(file_path, 'w+')
+
+        # Write statistical data to file
+        file.write("POP. SIZE: " + str(self.POPULATION_SIZE) + "\n")
+        file.write("AGENT.SIZE: " + str(self.AGENT_SIZE) + "\n")
+        file.write("GENERATIONS: " + str(self.NUMBER_OF_GENERATIONS) + "\n")
+        file.write("ELITE SIZE: " + str(self.ELITE_SIZE) + "\n")
+
+        file.write("BEST FITNESS\n")
+        file.write(self.DELIMITER)
+        for b in self.best_fitness:
+            file.write(str(b) + "\n")
+
+        file.write("WORST FITNESS\n")
+        file.write(self.DELIMITER)
+        for w in self.worst_fitness:
+            file.write(str(w) + "\n")
+
+        file.write("AVERAGE FITNESS\n")
+        file.write(self.DELIMITER)
+        for a in self.average_fitness:
+            file.write(str(a) + "\n")
+        file.close()
+
+    def save_agent(self, agent):
+        filename = './out/agent_' + str(self.POPULATION_SIZE) + '_' + \
+                   str(self.AGENT_SIZE) + '_' + str(self.NUMBER_OF_GENERATIONS) + '_' + \
+                   str(self.ELITE_SIZE) + '_' + str(agent.fitness) + '.txt'
+        
+        file = open(filename, 'w+')
+        file.write("".join(agent.to_program()))
+        file.flush()
+        print("".join(agent.to_program()))
 
 
-def save_agent(agent):
-    filename = './out/agent_' + str(population_size) + '_' + \
-               str(agent_size) + '_' + str(number_of_generations) + '_' + \
-               str(elite_size) + '_' + str(agent.fitness) + '.txt'
-    f = open(filename, 'w+')
-    f.write("".join(agent.to_program()))
-    f.flush()
-    print("".join(agent.to_program()))
+    def prepare_execution_break(self, date_time_string):
+        try:
+            dt = parse(date_time_string)
+            return True, dt
+        except ValueError:
+            return False, None
 
 
 def read_agent(filename):
@@ -47,71 +144,16 @@ def solve_with_best():
     agent.solve_mazes()
 
 
-def write_stats_to_file(file_path):
-    f = open(file_path, 'w+')
-    f.write("POP. SIZE\n" + str(population_size) + "\n")
-    f.write("AGENT.SIZE\n" + str(agent_size) + "\n")
-    f.write("GENERATIONS\n" + str(number_of_generations) + "\n")
-    f.write("ELITE SIZE\n" + str(elite_size) + "\n")
-    f.write("BEST FITNESS\n")
-    for b in best_fitness:
-        f.write(str(b) + "\n")
-    f.write("WORST FITNESS\n")
-    for w in worst_fitness:
-        f.write(str(w) + "\n")
-    f.write("AVERAGE FITNESS\n")
-    for a in avg_fitness:
-        f.write(str(a) + "\n")
-    f.close()
-
-
-def prepare_execution_break(date_time_string):
-    try:
-        dt = parse(date_time_string)
-        return True, dt
-    except ValueError:
-        return False, None
-
-
 if __name__ == "__main__":
-    start = time.time()
-    population = Population(population_size=population_size, agent_size=agent_size,
-                            elite_size=elite_size, ranking=Ranking.FITNESS)
-    population.generate_agents()
+    """ 
+    SAMPLE PARAMETERS
+    -----------------
+    population_size = 150
+    agent_size = 25
+    number_of_generations = 500
+    elite_size = 15
 
-    do_break_on_time, break_date_time = prepare_execution_break(execution_break)
-    padding = "=" * 26
-    i = 0
-    while i < number_of_generations \
-            and (not do_break_on_time or (do_break_on_time and datetime.now() < break_date_time)):
-        print "".join((padding, (" GENERATION " + str(i) + " "), padding))
-        fit_start = time.time()
-        fitness = [a.async_compute_fitness() for a in population.agents]
-        print("async_compute_fitness time: " + str(time.time() - fit_start))
-        population_avg = sum(fitness) / len(fitness)
-        for f in fitness:
-            print("Agent fitness: " + str(f))
-        agents_sorted = sorted(fitness)
-        population_best = agents_sorted[0]
-        population_worst = agents_sorted[len(agents_sorted) - 1]
-        print("Best population fitness: " + str(population_best))
-        print("Worst population fitness: " + str(population_worst))
-        print("Average population fitness: " + str(population_avg))
-        best_fitness.append(population_best)
-        worst_fitness.append(population_worst)
-        avg_fitness.append(population_avg)
-
-        population.evolve(debug=False)
-        i += 1
-
-    print("".join((padding, (" FINISHED " + str(i) + " "), padding)))
-    print("Execution time: " + str(time.time() - start))
-
-    [a.async_compute_fitness() for a in population.agents]
-    best = population.return_fittest(population.agents)
-    print("==== BEST AGENT ====")
-
-    stats_path = './out/run_stats.txt'
-    write_stats_to_file(stats_path)
-    save_agent(best)
-    best.solve_mazes()
+    """
+    print "Executing maze"
+    maze = MazeRunner(150, 25, 0, 15)
+    maze.execute()
