da76abeb24fdc2cb7cd9d881aa5bec13bfbb2a3e
1nfused <enigma4.speed@gmail.com>
2017-01-08 13:30:40 +0100

    Fixed merge conflict, added some better print formatting.

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ class MazeRunner(object):
             i += 1
 
         print("".join((padding, (" FINISHED " + str(i) + " "), padding)))
-        print("Execution time: " + str(time.time() - start))
+        print("Execution time: " + str(time.time() - start) + "\n")
 
         [a.async_compute_fitness() for a in population.agents]
         best = population.return_fittest(population.agents)
@@ -145,7 +145,6 @@ def solve_with_best():
     agent.solve_mazes()
 
 
-<<<<<<< HEAD
 if __name__ == "__main__":
     """ 
     SAMPLE PARAMETERS
@@ -157,76 +156,5 @@ if __name__ == "__main__":
 
     """
     print "Executing maze"
-    maze = MazeRunner(150, 25, 0, 15)
+    maze = MazeRunner(150, 25, 1, 15)
     maze.execute()
-=======
-def write_stats_to_file(file_path):
-    f = open(file_path, 'w+')
-    f.write("POP. SIZE\n" + str(population_size) + "\n")
-    f.write("AGENT.SIZE\n" + str(agent_size) + "\n")
-    f.write("GENERATIONS\n" + str(number_of_generations) + "\n")
-    f.write("ELITE SIZE\n" + str(elite_size) + "\n")
-    f.write("BEST FITNESS\n")
-    for b in best_fitness:
-        f.write(str(b) + "\n")
-    f.write("WORST FITNESS\n")
-    for w in worst_fitness:
-        f.write(str(w) + "\n")
-    f.write("AVERAGE FITNESS\n")
-    for a in avg_fitness:
-        f.write(str(a) + "\n")
-    f.close()
-
-
-def prepare_execution_break(date_time_string):
-    try:
-        dt = parse(date_time_string)
-        return True, dt
-    except ValueError:
-        return False, None
-
-
-def perform_maze_simulation():
-    start = time.time()
-    population = Population(population_size=population_size, agent_size=agent_size,
-                            elite_size=elite_size, ranking=Ranking.FITNESS)
-    population.generate_agents()
-    do_break_on_time, break_date_time = prepare_execution_break(execution_break)
-    padding = "=" * 26
-    i = 0
-    while i < number_of_generations \
-            and (not do_break_on_time or (do_break_on_time and datetime.now() < break_date_time)):
-        print "".join((padding, (" GENERATION " + str(i) + " "), padding))
-        fit_start = time.time()
-        fitness = [a.async_compute_fitness() for a in population.agents]
-        print("async_compute_fitness time: " + str(time.time() - fit_start))
-        population_avg = sum(fitness) / len(fitness)
-        for f in fitness:
-            print("Agent fitness: " + str(f))
-        agents_sorted = sorted(fitness)
-        population_best = agents_sorted[0]
-        population_worst = agents_sorted[len(agents_sorted) - 1]
-        print("Best population fitness: " + str(population_best))
-        print("Worst population fitness: " + str(population_worst))
-        print("Average population fitness: " + str(population_avg))
-        best_fitness.append(population_best)
-        worst_fitness.append(population_worst)
-        avg_fitness.append(population_avg)
-
-        population.evolve(debug=False)
-        i += 1
-    print("".join((padding, (" FINISHED " + str(i) + " "), padding)))
-    print("Execution time: " + str(time.time() - start))
-    [a.async_compute_fitness() for a in population.agents]
-    best = population.return_fittest(population.agents)
-    print("==== BEST AGENT ====")
-    stats_path = './out/run_stats.txt'
-    write_stats_to_file(stats_path)
-    save_agent(best)
-    best.solve_mazes()
-
-
-if __name__ == "__main__":
-    #perform_maze_simulation()
-    solve_with_best()
->>>>>>> dc478bc4572408f97538dfb9a0bb1b2a2fbdfa73
